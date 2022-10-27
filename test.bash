e7e5397a50ef2325645c4c35d0f8229c78a56b87
Nagamine Takuya <s21c1090ke@s.chibakoudai.jp>
2022-10-27 12:22:11 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Takuya Nagamine
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
 
-[ "${out}" = 14 ]
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
